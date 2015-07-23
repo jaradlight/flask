4895902781d4ff826041dc3af725a5792c12acbc
Jarad Light <jaradlight@gmail.com>
2015-07-23 12:16:47 -0700

    Use mocha for running tests while watching files. Keep existing test setup for Travis CI.

--- gulpfile.js
+++ gulpfile.js
@@ -4,12 +4,19 @@ var shell = require('gulp-shell');
 var path = require('path');
 var truesass = require('sass-true');
 
+var mocha = require('gulp-mocha');
+
 var sassFile = path.join(__dirname, 'test/test.scss');
 
 gulp.task('test', function() {
   truesass.runSass({file: sassFile}, function(){ return true; });
-})
+});
+
+gulp.task('test:mocha', function() {
+  return gulp.src('test/test.js', {read: false})
+    .pipe(mocha({reporter: 'spec'}));
+});
 
 gulp.task('default', function() {
-  gulp.watch(['sass/**/*.scss', 'test/**/*.scss'], ['test']);
+  gulp.watch(['sass/**/*.scss', 'test/**/*.scss'], ['test:mocha']);
 });
